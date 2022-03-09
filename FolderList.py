b686778d5ca2114827eb57e66c6d9e0106843a94
Daniel Walls <walls.dk@gmail.com>
2022-03-08 20:45:32 -0800

    moved a lot of logic for FolderList into its own file, made things overall simpler

---
+++ FolderList.py
@@ -0,0 +1,78 @@
+import os
+import sys
+import typing
+
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QWidget, QFileDialog, QListView, QAbstractItemView, QTreeView
+from Generated.FolderListGenerated import Ui_FolderListWidget
+
+class FolderList(QWidget):
+    def ui(self) -> Ui_FolderListWidget:
+        return self.__ui
+    
+    def __init__(self, 
+            parent: typing.Optional['QWidget'] = None, 
+            base: typing.Optional['QWidget'] = None,
+            flags: typing.Union[QtCore.Qt.WindowFlags, QtCore.Qt.WindowType] = QtCore.Qt.WindowFlags()
+    ) -> None:
+
+        # Convert our UI design into python code.
+        os.system("pyuic5 -x UI/FolderList.ui -o Generated/FolderListGenerated.py")
+
+        super().__init__(parent, flags)
+        self.__setupUI(base)
+        self.__setupEvents()
+
+    def __setupUI(self, base: QWidget) -> None:
+        self.__ui = Ui_FolderListWidget()
+        self.__ui.setupUi(base if base != None else self)
+
+    def __setupEvents(self) -> None:
+        ui = self.ui()
+        ui.FolderList.itemSelectionChanged.connect(lambda: self.__handleSelectionChange(ui))
+        ui.ButtonRemoveFolder.clicked.connect(lambda: self.__removeFolder(ui))
+        ui.ButtonAddFolder.clicked.connect(lambda: self.__addFolder(ui))
+
+    def __handleSelectionChange(self, ui: Ui_FolderListWidget):
+        selectedItems = ui.FolderList.selectedItems()
+        ui.ButtonRemoveFolder.setEnabled(len(selectedItems) > 0)
+
+    def __addFolder(self, ui: Ui_FolderListWidget):
+        """ 
+        TODO: Slightly jank multi-select system take from here: 
+        TODO: https://stackoverflow.com/questions/38252419/how-to-get-qfiledialog-to-select-and-return-multiple-folders
+        TODO: If this starts causing problems, we might want to just revert to the vanilla QFileDialog system.
+        """
+        file_dialog = QFileDialog()
+        file_dialog.setFileMode(QFileDialog.DirectoryOnly)
+        file_dialog.setOption(QFileDialog.DontUseNativeDialog, True)
+        file_view = file_dialog.findChild(QListView, 'listView')
+
+        if file_view:
+            file_view.setSelectionMode(QAbstractItemView.ExtendedSelection)
+        f_tree_view = file_dialog.findChild(QTreeView)
+        if f_tree_view:
+            f_tree_view.setSelectionMode(QAbstractItemView.ExtendedSelection)
+
+        if file_dialog.exec():
+            paths = file_dialog.selectedFiles()
+
+        for path in paths:
+            ui.FolderList.addItem(path)
+
+    def __removeFolder(self, ui: Ui_FolderListWidget):
+        folderList = ui.FolderList
+        indexes = folderList.selectedIndexes()
+        indexes.reverse()
+
+        for index in indexes:
+            folderList.takeItem(index.row())
+
+# If you run this file on its own, show the folder list for debugging.
+if __name__ == "__main__":
+    application = QtWidgets.QApplication(sys.argv)
+
+    folderList = FolderList()
+    folderList.show()
+
+    sys.exit(application.exec_())
--- Settings.py
+++ Settings.py
@@ -1,15 +1,11 @@
 import os
 import sys
 import typing
-
-# Convert our UI design into python code.
-os.system("pyuic5 -x UI/FolderList.ui -o Generated/FolderListGenerated.py")
-os.system("pyuic5 -x UI/Settings.ui -o Generated/SettingsGenerated.py")
+from FolderList import FolderList
 
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import QWidget, QDialog
 
-# Import generated UI code.
 from Generated.SettingsGenerated import Ui_SettingsDialog
 from Generated.FolderListGenerated import Ui_FolderListWidget
 
@@ -20,35 +16,29 @@ class Settings(QDialog):
             flags: typing.Union[QtCore.Qt.WindowFlags, QtCore.Qt.WindowType] = QtCore.Qt.WindowFlags()
     ) -> None:
 
+        # Convert our UI design into python code.
+        os.system("pyuic5 -x UI/FolderList.ui -o Generated/FolderListGenerated.py")
+        os.system("pyuic5 -x UI/Settings.ui -o Generated/SettingsGenerated.py")
+
         super().__init__(parent, flags)
+        self.__setupUI()
+
+    def __setupUI(self) -> None:
 
         ui = Ui_SettingsDialog()
-        self.__setupUI(ui)
-        self.__setupEvents(ui)
-
-    def __setupUI(self, ui: Ui_SettingsDialog) -> None:
-
         ui.setupUi(self)
-        self.projectSearchWidget = self.__setupSearchFolder(title = "Project Search Folders", baseWidget = ui.ProjectFolderList)
-        self.editorSearchWidget = self.__setupSearchFolder(title = "Editor Search Folders", baseWidget = ui.EditorFolderList)
+        
+        self.__setupSearchFolder(title = "Project Search Folders", baseWidget = ui.ProjectFolderList)
+        self.__setupSearchFolder(title = "Editor Search Folders", baseWidget = ui.EditorFolderList)
 
     def __setupSearchFolder(self, 
             baseWidget: QWidget,
             title: str = "Search Folder"
     ) -> Ui_FolderListWidget:
 
-        folderWidget = Ui_FolderListWidget()
-        folderWidget.setupUi(baseWidget)
-        folderWidget.GroupProjectFolders.setTitle(title)
-
+        folderWidget = FolderList(base = baseWidget)
+        folderWidget.ui().GroupProjectFolders.setTitle(title)
         return folderWidget
-
-    def __setupEvents(self, ui: Ui_SettingsDialog) -> None:
-        self.projectSearchWidget.ButtonAddFolder.clicked.connect(lambda: print("Add project folder"))
-        self.projectSearchWidget.ButtonRemoveFolder.clicked.connect(lambda: print("Remove project folder"))
-        self.editorSearchWidget.ButtonAddFolder.clicked.connect(lambda: print("Add editor folder"))
-        self.editorSearchWidget.ButtonRemoveFolder.clicked.connect(lambda: print("Remove editor folder"))
-
 
 # If you run this file on its own, show the settings for debugging.
 if __name__ == "__main__":
