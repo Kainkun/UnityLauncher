a04a0afd919c41ffa4d54e5c43397499dc2d1610
Daniel Walls <walls.dk@gmail.com>
2022-03-09 11:39:31 -0800

    added documentation and cleaned up Config.py

--- Config.py
+++ Config.py
@@ -3,53 +3,60 @@ import json
 import typing
 
 class Config():
+    """ 
+        Handles the serialization and deserialization of persistent data for the UnityLauncher.
 
-    # TODO: better documentation
+        - Notes:
+            - Follows the MonoState pattern: https://wiki.c2.com/?MonostatePattern, so 
+            keep in mind there is hidden static state associated with this class.
+    """
 
-    configData: typing.Dict = {}
-    configName: str = "config.json"
+    __configData: typing.Dict = {}
+    __configName: str = "config.json"
+    __projectFoldersKey = "ProjectFolders"
+    __editorFoldersKey = "EditorFolders"
 
-    # Keys used for accessing and storing data in the JSON file.
+    # Getter-Setter for Project Folders
+    
+    def setProjectFolders(self, folders: typing.List[str]) -> None:
+        self.__configData[self.__projectFoldersKey] = folders
 
-    projectFoldersKey = "ProjectFolders"
-    editorFoldersKey = "EditorFolders"
+    def getProjectFolders(self) -> typing.List[str]:
+        return self.__configData[self.__projectFoldersKey]
+
+    # Getter-Setter for Editor Folders
+
+    def setEditorFolders(self, folders: typing.List[str]) -> None:
+        self.__configData[self.__editorFoldersKey] = folders
+
+    def getEditorFolders(self) -> typing.List[str]:
+        return self.__configData[self.__editorFoldersKey]
+
+    # Serialization
+
+    def writeChanges(self):
+        with open(self.__configName, mode = "w+") as configFile:
+            json.dump(self.__configData, configFile, indent=4, sort_keys=True)
+
+    def readChanges(self):
+        with open(self.__configName) as configFile:
+            self.__configData = json.load(configFile)
 
     def __init__(self) -> None:
 
-        if not os.path.exists(self.configName):
-            self.configData = self.__createDefaultConfig()
+        """ Handles the serialization and deserialization of persistent data for the UnityLauncher. """
+
+        if not os.path.exists(self.__configName):
+            self.__configData = self.__createDefaultConfig()
             self.writeChanges()
                 
         self.readChanges()
 
     def __createDefaultConfig(self) -> typing.Dict:
+
+        """ Creates the default JSON structure for first-time users. """
+
         return {
-            self.projectFoldersKey : [],
-            self.editorFoldersKey : []
+            self.__projectFoldersKey : [],
+            self.__editorFoldersKey : []
         }
-
-    # Getter-Setter for Project Folders
-    
-    def setProjectFolders(self, folders: typing.List[str]) -> None:
-        self.configData[self.projectFoldersKey] = folders
-
-    def getProjectFolders(self) -> typing.List[str]:
-        return self.configData[self.projectFoldersKey]
-
-    # Getter-Setter for Editor Folders
-
-    def setEditorFolders(self, folders: typing.List[str]) -> None:
-        self.configData[self.editorFoldersKey] = folders
-
-    def getEditorFolders(self) -> typing.List[str]:
-        return self.configData[self.editorFoldersKey]
-
-    # Serialization
-
-    def writeChanges(self):
-        with open(self.configName, mode = "w+") as configFile:
-            json.dump(self.configData, configFile, indent=4, sort_keys=True)
-
-    def readChanges(self):
-        with open(self.configName) as configFile:
-            self.configData = json.load(configFile)
